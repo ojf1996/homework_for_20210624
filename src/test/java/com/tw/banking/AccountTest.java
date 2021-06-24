632cf7747c07f5aa55b8b2cde03482dda507e34e
ojf1996 <247057911@qq.com>
2021-06-24 20:10:12 +0800

    [jimmy ou] add test case for Account: should_printer_print_transactionMock_when_printStatement_given_allTransactions_from_transactionRepository_is_transactionMock

--- src/test/java/com/tw/banking/AccountTest.java
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -1,7 +1,12 @@
 package com.tw.banking;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class AccountTest {
@@ -12,6 +17,7 @@ class AccountTest {
         TransactionRepository transactionRepositoryMock = mock(TransactionRepository.class);
         Printer printerMock = mock(Printer.class);
         Account account = new Account(transactionRepositoryMock, printerMock);
+        //when
         account.deposit(10);
         //then
         verify(transactionRepositoryMock, times(1)).addDeposit(10);
@@ -27,5 +33,23 @@ class AccountTest {
         account.withdraw(10);
         //then
         verify(transactionRepositoryMock, times(1)).addWithdraw(10);
+    }
+
+    @Test
+    public void should_printer_print_transactionMock_when_printStatement_given_allTransactions_from_transactionRepository_is_transactionMock() {
+        //given
+        TransactionRepository transactionRepositoryMock = mock(TransactionRepository.class);
+        Printer printerMock = mock(Printer.class);
+        Transaction transactionMock = mock(Transaction.class);
+        Account account = new Account(transactionRepositoryMock, printerMock);
+        when(transactionRepositoryMock.allTransactions()).thenReturn(Collections.singletonList(transactionMock));
+        ArgumentCaptor<List<Transaction>> captor = ArgumentCaptor.forClass(List.class);
+        //when
+        account.printStatement();
+        //then
+        verify(printerMock, times(1)).print(captor.capture());
+        List<Transaction> argumentList = captor.getValue();
+        assertEquals(1, argumentList.size());
+        assertEquals(transactionMock, argumentList.get(0));
     }
 }
