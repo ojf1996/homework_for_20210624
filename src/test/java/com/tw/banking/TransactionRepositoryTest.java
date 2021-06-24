4ef32308614f71ddffb37eb06b9fd756d2dd50c2
ojf1996 <247057911@qq.com>
2021-06-24 20:23:58 +0800

    [jimmy ou] add test case for TransactionRepository: should_add_transaction_with_date_01012021_amount_is_minus_10_to_transactions_when_addDeposit_given_today_is_01012021_and_amount_is_10

--- src/test/java/com/tw/banking/TransactionRepositoryTest.java
+++ src/test/java/com/tw/banking/TransactionRepositoryTest.java
@@ -21,4 +21,18 @@ class TransactionRepositoryTest {
         assertEquals("01/01/2021", transactionRepository.transactions.get(0).date());
         assertEquals(10, transactionRepository.transactions.get(0).amount());
     }
+
+    @Test
+    public void should_add_transaction_with_date_01012021_amount_is_minus_10_to_transactions_when_addDeposit_given_today_is_01012021_and_amount_is_10() {
+        //given
+        Clock clockMock = mock(Clock.class);
+        TransactionRepository transactionRepository = new TransactionRepository(clockMock);
+        when(clockMock.todayAsString()).thenReturn("01/01/2021");
+        //when
+        transactionRepository.addWithdraw(10);
+        //then
+        assertEquals(1, transactionRepository.transactions.size());
+        assertEquals("01/01/2021", transactionRepository.transactions.get(0).date());
+        assertEquals(-10, transactionRepository.transactions.get(0).amount());
+    }
 }
