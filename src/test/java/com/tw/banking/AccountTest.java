0e48665e9aeda7e8c9759c4d6c4bbccb6b344b17
ojf1996 <247057911@qq.com>
2021-06-24 19:58:07 +0800

    [jimmy ou] add test case for Account: should_transactionRepository_addWithdraw_10_when_withdraw_given_amount_is_10; rename test case should_transactionRepository_add_10_when_deposit_given_amount_is_10 to should_transactionRepository_addDeposit_10_when_deposit_given_amount_is_10

--- src/test/java/com/tw/banking/AccountTest.java
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -2,20 +2,30 @@ package com.tw.banking;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class AccountTest {
 
     @Test
-    public void should_transactionRepository_add_10_when_deposit_given_amount_is_10() {
+    public void should_transactionRepository_addDeposit_10_when_deposit_given_amount_is_10() {
+        //given
+        TransactionRepository transactionRepositoryMock = mock(TransactionRepository.class);
+        Printer printerMock = mock(Printer.class);
+        Account account = new Account(transactionRepositoryMock, printerMock);
+        account.deposit(10);
+        //then
+        verify(transactionRepositoryMock, times(1)).addDeposit(10);
+    }
+
+    @Test
+    public void should_transactionRepository_addWithdraw_10_when_withdraw_given_amount_is_10() {
         //given
         TransactionRepository transactionRepositoryMock = mock(TransactionRepository.class);
         Printer printerMock = mock(Printer.class);
         Account account = new Account(transactionRepositoryMock, printerMock);
         //when
-        account.deposit(10);
+        account.withdraw(10);
         //then
-        verify(transactionRepositoryMock, times(1)).addDeposit(10);
+        verify(transactionRepositoryMock, times(1)).addWithdraw(10);
     }
 }
