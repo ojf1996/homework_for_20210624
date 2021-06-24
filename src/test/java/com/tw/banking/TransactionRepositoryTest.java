9199fe9d4c5170aa69b291cce0fa08045bd31bcd
ojf1996 <247057911@qq.com>
2021-06-24 20:26:49 +0800

    [jimmy ou] add test case for TransactionRepository: should_return_transaction_with_date_01012021_amount_is_minus_10_to_transactions_when_allTransactions_given_only_one_transaction_with_today_is_01012021_and_amount_is_10

--- src/test/java/com/tw/banking/TransactionRepositoryTest.java
+++ src/test/java/com/tw/banking/TransactionRepositoryTest.java
@@ -2,6 +2,8 @@ package com.tw.banking;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -34,5 +36,20 @@ class TransactionRepositoryTest {
         assertEquals(1, transactionRepository.transactions.size());
         assertEquals("01/01/2021", transactionRepository.transactions.get(0).date());
         assertEquals(-10, transactionRepository.transactions.get(0).amount());
+    }
+
+    @Test
+    public void should_return_transaction_with_date_01012021_amount_is_minus_10_to_transactions_when_allTransactions_given_only_one_transaction_with_today_is_01012021_and_amount_is_10() {
+        //given
+        Clock clockMock = mock(Clock.class);
+        TransactionRepository transactionRepository = new TransactionRepository(clockMock);
+        when(clockMock.todayAsString()).thenReturn("01/01/2021");
+        transactionRepository.addWithdraw(10);
+        //when
+        List<Transaction> transactions = transactionRepository.allTransactions();
+        //then
+        assertEquals(1, transactions.size());
+        assertEquals("01/01/2021", transactions.get(0).date());
+        assertEquals(-10, transactions.get(0).amount());
     }
 }
